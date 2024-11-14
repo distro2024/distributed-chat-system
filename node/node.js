3b5d776d8deb35964f0b13a5d11d7c34e753ded5
Heidi Holappa <heidi.holappa@helsinki.fi>
2024-11-14 21:31:35 +0200

    update bully algorithm implementation

--- node/node.js
+++ node/node.js
@@ -21,15 +21,22 @@ app.use(express.json())
 const nodeId = uuidv4()
 
 // For now, node1 is always the leader based on the IS_LEADER environment variable
-let isLeader = IS_LEADER // testing
-let leaderId = IS_LEADER ? nodeId : null
-let leaderAddress = IS_LEADER ? NODE_HOST : null
-let leaderPublicAddress = IS_LEADER ? PUBLIC_HOST : null
 
+let isCoordinator = IS_LEADER // testing
+let coordinatorId = isCoordinator ? nodeId : null
+let coordinatorAddress = IS_LEADER ? NODE_HOST : null
+let coordinatorPublicAddress = IS_LEADER ? PUBLIC_HOST : null
+
+// when an election is initiated, this node becomes a candidate
+// if it receives an OK from a higher priority node, it will not be a candidate anymore
+let isCandidate = false
+
+
+// list of nodes in the network
 let nodes = []
-let messages = []
-let isCoordinator = false
-let electionVotes = []
+// list of chat-messages in the network
+let discussion = []
+
 
 app.post("/register_node", (req, res) => {
   const { nodeId, nodeAddress, publicAddress } = req.body
@@ -41,17 +48,17 @@ app.post("/register_node", (req, res) => {
 })
 
 const sendHeartbeatToDirector = async () => {
-  if (isLeader) {
+  if (isCoordinator) {
     try {
       await axios.post(`${DIRECTOR_URL}/register_leader`, {
-        leaderId,
-        leaderAddress,
-        leaderPublicAddress,
+        coordinatorId,
+        leaderAddress: coordinatorAddress,
+        leaderPublicAddress: coordinatorPublicAddress,
       })
       console.log("Heartbeat sent to Director")
     } catch (error) {
       console.error("Error sending heartbeat to Director:", error.message)
-      isLeader = false
+      isCoordinator = false
       initiateElection()
     }
   }
@@ -71,16 +78,21 @@ const registerWithDirector = async () => {
 }
 
 /**
+ * NOTE: not yet implemented correctly
  * Send a message to all nodes to elect a new coordinator
  * @param {ChatNode} chatNode - The chat node instance for this client
  */
 const initiateElection = async () => {
+  isCandidate = true
   nodes.forEach((node) => {
     // Sends a message through a web socket to a node
     // requesting a vote for a new coordinator
-    axios.post(`${node.nodeAddress}/election`, {
-      id: nodeId,
-    })
+    if (node.nodeId > nodeId) {
+      const socket = ioClient(node.nodeAddress);
+      socket.emit('election', { nodeId });
+    }
+
+
   })
   // Wait for three seconds to receive votes from other nodes
   setTimeout(determineVotingOutcome, 3000)
@@ -88,43 +100,44 @@ const initiateElection = async () => {
 }
 
 /**
- * Handle an incoming election vote from another node
+* NOTE: not yet implemented correctly 
+* Handle an incoming election vote from another node
  * Web socket will be implemented in the future
  * @param {any} node - The node that sent the vote
  */
-const submitVote = async (node) => {
-  // TODO: how to handle multiple votes from the same node?
-  // TODO: how to listen to incoming messages to different routes
-  this.electionVotes.push(node.id)
+const submitVote = async () => {
+  // listen to incoming messages to the route /vote 
+  // this should be implemented with web socket in the future
+  // this is just the basic idea
+  app.post('/vote', (req, res) => {
+    const { id } = req.body;
+    // if id is in the list of nodes, this node is not a candidate anymore
+    if (nodes.includes(id)) {
+      isCandidate = false;
+    }
+  });
+  
 }
 
 /**
- * Determine the outcome of the election based
+* NOTE: not yet implemented correctly 
+* Determine the outcome of the election based
  * on the selected election algorithm
  * @param {ChatNode} chatNode - The chat node instance for this client
  */
 const determineVotingOutcome = async () => {
-  // Order ids of nodes that submitted a vote in ascending order and
-  // select the first element as the winner of the election
-  const winner = this.electionVotes.sort()[0]
-  // if the winner is this node, set the coordinator flag to true
-  if (winner === this.id) {
+  if (isCandidate) {
     this.isCoordinator = true
     // send a message to all nodes to update their coordinator
     nodes.forEach((node) => {
-      axios.post(`${node.nodeAddress}/update-coordinator`, {
-        id: nodeId,
-      })
+      const socket = ioClient(node.nodeAddress);
+      socket.emit('update-coordinator', { nodeId });
     })
-  } else {
-    // send a message to the winner to initate a new election
-    axios.post(`${winner.nodeAddress}/initiate-election`, {
-      id: nodeId,
-    })
-  }
+
+  } 
 }
 
-if (isLeader) {
+if (isCoordinator) {
   sendHeartbeatToDirector()
 }
 
@@ -134,7 +147,7 @@ setInterval(sendHeartbeatToDirector, 5000)
 
 server.listen(PORT, () => {
   console.log(`Node service is running on port ${PORT}`)
-  if (!leaderId) {
+  if (!isCoordinator) {
     // TODO initiateElection();
   }
 })
